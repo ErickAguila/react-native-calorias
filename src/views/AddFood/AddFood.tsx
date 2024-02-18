ca414721d88df1d0bea0ac71bbf1bcd393702d07
ext-eaguila_segchile <ext_eaguila@banmedica.cl>
2024-02-18 05:21:40 -0300

    Crear componente de filtro en pantalla detalle

--- src/views/AddFood/AddFood.tsx
+++ src/views/AddFood/AddFood.tsx
@@ -1,14 +1,69 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Header from '../../components/Header';
+import {Button, Icon, Input} from '@rneui/themed';
 
 const AddFood = () => {
   return (
-    <View>
-      <Text>Add Food</Text>
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.addFoodContainer}>
+        <View style={styles.legendContainer}>
+          <Text style={styles.addFoddLegend}>Add Food</Text>
+        </View>
+        <View style={styles.addFoddBtnContainer}>
+          <Button
+            icon={<Icon name="add-circle-outline" color="#fff" />}
+            radius="lg"
+            color="#4ecb71"
+          />
+        </View>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.inputContainer}>
+          <Input placeholder="apples, pie, soda.." />
+        </View>
+        <Button
+          title="Search"
+          color="#ade8af"
+          titleStyle={styles.searchBtnTile}
+          radius="lg"
+        />
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+  },
+  legendContainer: {
+    flex: 1,
+  },
+  addFoddBtnContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  addFoodContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 24,
+  },
+  addFoddLegend: {
+    fontSize: 20,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+  },
+  inputContainer: {
+    flex: 1,
+    marginLeft: -12,
+  },
+  searchBtnTile: {
+    color: '#FFF',
+    fontSize: 14,
+  },
+});
 
 export default AddFood;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 
 import Header from '../../components/Header';
+import {Button, Icon} from '@rneui/themed';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParams} from '../../types';
 
 const Home = () => {
+  const {navigate} =
+    useNavigation<NativeStackNavigationProp<RootStackParams, 'Home'>>();
+  const handleAddCaloriesPress = () => {
+    navigate('AddFood');
+  };
+
   return (
     <View style={styles.container}>
       <Header />
+      <View style={styles.caloriesContainer}>
+        <View style={styles.leftContainer}>
+          <Text style={styles.caloriesLegend}>Calories</Text>
+        </View>
+        <View style={styles.rightContainer}>
+          <Button
+            icon={<Icon name="add-circle-outline" color="#fff" />}
+            radius="lg"
+            color="#4ecb71"
+            onPress={handleAddCaloriesPress}
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -16,6 +39,23 @@ const styles = StyleSheet.create({
     padding: 12,
     backgroundColor: '#FFF',
     flex: 1,
+  },
+  caloriesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 24,
+  },
+  leftContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  rightContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  caloriesLegend: {
+    fontSize: 20,
   },
 });
 
